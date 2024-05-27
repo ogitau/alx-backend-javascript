514bab3da7e21b72e5002f91b7f5ea1d8e7ab595
Owen Gitau <owengitau02@gmail.com>
2024-05-27 23:37:55 +0300

    Employees object

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const my_array = [];
+    for (const item of array) {
+      my_array.push(appendString + item);
+    }
+
+    return my_array;
+  }
