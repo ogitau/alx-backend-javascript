b45ef820f57301ecafbbda89901f8cd1d2373554
Owen Gitau <owengitau02@gmail.com>
2024-05-30 00:39:48 +0300

    fixed errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup() {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  const result = [];
-  return promises.allSettled(promises).then((results) => {
-    results.map(({ status, value, reason }) => result.push({
-      status,
-      value: status === 'rejected' ? reason.toString() : value,
-    }));
-    return result;
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
